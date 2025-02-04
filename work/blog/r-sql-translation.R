d2574764e93e1a08fded8732e465e4aaba7c1b21
14katsumix <14katsumix@gmail.com>
2025-02-04 22:22:57 +0900

    commit

--- work/blog/r-sql-translation.R
+++ work/blog/r-sql-translation.R
@@ -5,49 +5,42 @@ library(dbplyr)
 library(duckdb)
 library(tibble)
 
+# サンプルのデータフレームを作成
+df_sales = tribble(
+  ~store, ~month, ~sales, ~profit,
+  "S001",  7L,     15000,  3000,
+  "S001",  8L,     16000,  3200,
+  "S002",  7L,     14000,  2800,
+  "S002",  8L,     19000,  NA,
+  "S003",  7L,     12000,  2500,
+  "S003",  8L,     13000,  2600,
+  "S004",  7L,     20000,  3800,
+  "S004",  8L,     21000,  4200,
+  "S005",  7L,     16000,  3300,
+  "S005",  8L,     17000,  3400
+)
+
+df_master = tribble(
+  ~store, ~name,    ~pref, 
+  "S001", "storeA", "Tokyo",
+  "S002", "storeB", "Osaka",
+  "S003", "storeC", "Kanagawa",
+  "S004", "storeD", "Fukuoka", 
+  "S005", "storeE", "Chiba",
+  "S006", "storeF", "Hokkaido",
+  "S007", "storeG", "Saitama"
+)
+
 # DuckDB に接続 (一時データベース)
 con = DBI::dbConnect(duckdb::duckdb())
 
-# サンプルのデータフレームを作成
-df1 = tribble(
-  ~store, ~month, ~sales, ~profit,
-  "S001",  7,     15000,  3000,
-  "S001",  8,     16000,  3200,
-  "S002",  7,     18000,  3500,
-  "S002",  8,     19000,  3600,
-  "S003",  7,     12000,  2500,
-  "S003",  8,     13000,  2600,
-  "S004",  7,     20000,  3800,
-  "S004",  8,     21000,  4200,
-  "S005",  7,     16000,  3300,
-  "S005",  8,     17000,  3400
-)
-
-df2 = tribble(
-  ~store, ~name,    ~pref, 
-  "S001", "storeA", "tokyo",
-  "S002", "storeB", "osaka",
-  "S003", "storeC", "kanagawa",
-  "S004", "storeD", "fukuoka", 
-  "S005", "storeE", "chiba",
-  "S006", "storeF", "hokkaido",
-  "S007", "storeG", "saitama"
-)
-
-print(df2)
-
 # テーブルとしてデータベースに登録
 DBI::dbWriteTable(
-  con, "store_sales", df1, overwrite = TRUE
+  con, "store_sales", df_sales, overwrite = TRUE
 )
 DBI::dbWriteTable(
-  con, "store_master", df2, overwrite = TRUE
+  con, "store_master", df_master, overwrite = TRUE
 )
-
-# 
-db_sales %>% show_query()
-
-# "month" がダブルクォートで括られてるのは、これが duckdb の予約語だからです。
 
 # store_sales テーブルを dplyr で参照
 db_sales = tbl(con, "store_sales")
@@ -55,12 +48,14 @@ db_master = tbl(con, "store_master")
 
 #...............................................................................
 
+# 
+db_sales %>% show_query()
+
 ### dplyr 操作全体の変換
 
-## 単一テーブル操作
+## 単一テーブルの操作
 
-# select()
-# select(), SELECT句を修正します。
+# `select()` は `SELECT` 句を修正します: 
 db_sales %>% 
   select(store, sales) %>% 
   show_query()
@@ -73,7 +68,9 @@ db_sales %>%
   relocate(profit, sales, .after = store) %>% 
   show_query()
 
-# mutate() は SELECT 句を修正します。
+# "month" がダブルクォートで括られてるのは、これが duckdb の予約語だからです。
+
+# `mutate()` は `SELECT` 句を修正します: 
 db_sales %>% 
   mutate(margin = 100 * profit / sales, .keep = "unused") %>% 
   show_query()
@@ -106,7 +103,7 @@ db_sales %>%
   head(3) %>% 
   show_query()
 
-## 2つのテーブル操作
+## 2つのテーブルの操作
 
 # left_join() LEFT JOIN 句を生成します
 db_sales %>% 
@@ -177,7 +174,118 @@ db_sales %>%
 
 ### dplyr 操作内の式の変換
 
+## 算術演算子
+db_sales %>% 
+  mutate(
+    v1 = sales + profit, 
+    v2 = sales - profit, 
+    v3 = profit / sales, 
+    v4 = profit * sales, 
+    v5 = profit ^ 2L, 
+    .keep = "none"
+  ) %>% 
+  show_query()
 
+## 比較演算子、ブール演算子(&, |)
+db_sales %>% 
+  mutate(
+    v1 = (sales == 15000), 
+    v2 = (!(sales > 15000)), 
+    v3 = (sales != 15000 & profit >= 3000), 
+    v4 = (sales < 15000 | profit <= 3000), 
+    v5 = (store %in% c("S001", "S002")), 
+    .keep = "used"
+  ) %>% 
+  show_query()
+
+## 数学関数、数値の丸め
+db_sales %>% 
+  mutate(
+    v1 = log(profit), 
+    v2 = sqrt(profit), 
+    v3 = sin(profit), 
+    v4 = floor(profit / sales), 
+    .keep = "none"
+  ) %>% 
+  show_query()
+
+## 型変換
+db_sales %>% 
+  mutate(
+    m = as.character(month), 
+    .keep = "used"
+  ) %>% 
+  show_query()
+
+## 文字列操作
+db_master %>% 
+  mutate(
+    len = nchar(pref), 
+    upp = toupper(pref), 
+    sub = substr(name, 6, 6), 
+    p = paste(name, pref, sep = "-"), 
+    .keep = "used"
+  ) %>% 
+  show_query()
+
+## 日付操作
+db_master %>% 
+  mutate(
+    ymd = lubridate::as_date("2025-04-01"), # サンプルの作成
+    .keep = "used"
+  ) %>% 
+  head(1) %>% 
+  mutate(
+    strftime = strftime(ymd, "%Y/%m/%d"), 
+    month = lubridate::month(ymd), 
+    add = ymd + lubridate::days(7L), 
+    .keep = "used"
+  ) %>% 
+  show_query(cte = T)
+
+## パターンマッチング
+db_master %>% 
+  filter(
+    stringr::str_detect(pref, "ka$")
+  ) %>% 
+  show_query()
+
+## is.na()
+db_sales %>% 
+  filter(is.na(profit)) %>% 
+  show_query()
+
+## distinct()
+db_sales %>% 
+  distinct(month) %>% 
+  show_query()
+
+## if_else()
+db_sales %>% 
+  mutate(
+    s = if_else(profit > 3000, "big", "small", "none"), 
+    .keep = "used"
+  ) %>% 
+  show_query()
+
+
+mean, median
+rank, ntile
+lag, lead
+cumsum, cummean
+
+translate_sql(sql("x!"), con = con)
+translate_sql(x == sql("ANY VALUES(1, 2, 3)"), con = con)
+
+# dbplyr の変換は完璧ではありません。
+# SQLは正しくても、簡潔ではないSQLクエリに変換されるケース
+!is.na()
+
+
+store
+month
+sales
+profit
 
 #-------------------------------------------------------------------------------
 # カスタムメソッドを定義
